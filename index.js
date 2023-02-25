e2c8353298002b8fc495791bce23bfae66c90f1e
Benjamin Nneji <nnejiben@gmail.com>
2023-02-25 10:50:23 +0100

    complete

--- index.js
+++ index.js
@@ -1,38 +1,21 @@
+const chars = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+    "/"]
 
 let passwordEl1 = document.getElementById("password-el1")
 let passwordEl2 = document.getElementById("password-el2")
 
-function createPassword() {
-    const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-    "/"]
-    let passwordLength = 15
-    let password1 = ""
-    for (let i = 0; i <= passwordLength; i++) {
-        let randomCharacter = Math.floor(Math.random() * characters.length)
-        password1 += characters.substring(randomCharacter, randomCharacter +1)
+let passwordLength = 15
+
+function generatePassword() {
+    
+    passwordEl1.textContent = ""
+    passwordEl2.textContent = ""
+
+    for (let i = 0; i < passwordLength; i++) {
+        randomStr1 = Math.floor(Math.random() * chars.length)
+        randomStr2 = Math.floor(Math.random() * chars.length)
+
+        passwordEl1.textContent += chars[randomStr1]
+        passwordEl2.textContent += chars[randomStr2]
+    }
 }
-    // document.getElementById(password-el1),value = password1
-}
-
-
-
-// var password=document.getElementById("password");
-
-//  function genPassword() {
-//     var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//     var passwordLength = 12;
-//     var password = "";
-//  for (var i = 0; i <= passwordLength; i++) {
-//    var randomNumber = Math.floor(Math.random() * chars.length);
-//    password += chars.substring(randomNumber, randomNumber +1);
-//   }
-//         document.getElementById("password").value = password;
-//  }
-
-// function copyPassword() {
-//   var copyText = document.getElementById("password");
-//   copyText.select();
-//   document.execCommand("copy");  
-// }
-
-
